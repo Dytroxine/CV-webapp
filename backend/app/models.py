a185500196d72e0c0a19ab9ce71a5cfbf34c150c
hohoryashka <danilka992zah@gmail.com>
2025-09-03 00:31:32 +0300

    Some UI/UX

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
-import datetime
 
 Base = declarative_base()
 
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,5 +1,4 @@
-import datetime
-
+from datetime import datetime
 from pydantic import BaseModel, EmailStr, ConfigDict
 
 class UserBase(BaseModel):
@@ -34,5 +33,4 @@ class ResumeImprovement(ResumeImprovementBase):
     resume_id: int
     created_at: datetime
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
